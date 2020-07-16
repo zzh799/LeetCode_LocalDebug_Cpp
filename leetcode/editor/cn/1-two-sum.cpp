23be2fec96971781e0dabfd4f5f39b1fb552c3f5
zzh799 <799828242@qq.com>
2020-07-16 20:24:10 +0800

    init

---
+++ leetcode/editor/cn/1-two-sum.cpp
@@ -0,0 +1,50 @@
+//给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。 
+//
+// 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。 
+//
+// 
+//
+// 示例: 
+//
+// 给定 nums = [2, 7, 11, 15], target = 9
+//
+//因为 nums[0] + nums[1] = 2 + 7 = 9
+//所以返回 [0, 1]
+// 
+// Related Topics 数组 哈希表 
+// 👍 8659 👎 0
+
+#include "include/headers.h"
+
+using namespace std;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> map;
+        vector<int> result;
+        for(int i =0;i<nums.size();i++){
+            int complement = target-nums[i];
+
+            if(map.find(complement)!=map.end()){
+                result={map[complement],i};
+                break;
+            }
+            map[nums[i]]=i;
+        }
+
+        return result;
+    }
+};
+//leetcode submit region end(Prohibit modification and deletion)
+
+
+int main()
+{
+    Solution s;
+    vector<int> data{7, 1, 5, 3, 6, 4};
+    //vector<int> ans = s.twoSum(data,11);
+    //cout << ans[0]<<ans[1]<<endl;
+    cout<<"Hello LeetCode"<<endl;
+}
---
+++ leetcode/editor/cn/include/headers.h
@@ -0,0 +1,45 @@
+/**
+ * 
+ * Copyright (c) 2019 NUAA Jiang Yang
+ * 
+ * @file
+ * @author   Jiang Yang (pokerpoke@qq.com)
+ * @date     2018-08
+ * @brief    
+ * @version  0.0.1
+ * 
+ * Last Modified:  2019-08-28
+ * Modified By:    Jiang Yang (pokerpoke@qq.com)
+ * 
+ */
+#ifndef _HEADERS_H_
+#define _HEADERS_H_
+
+// c++ stl libraries
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <set>
+#include <unordered_set>
+#include <unordered_map>
+#include <stack>
+#include <queue>
+#include <numeric>
+
+// c libraries
+#include <cctype>
+#include <climits>
+#include <cmath>
+#include <cstring>
+
+
+// listnode
+#include "listnode.h"
+#include "btree.h"
+
+using namespace std;
+using namespace LeetCode;
+
+#endif // !_HEADERS_H_

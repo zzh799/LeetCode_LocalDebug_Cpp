0b89e15279bbc5c196a2850240767a88930b9a40
zeaven <799828242@qq.com>
2021-01-20 18:43:24 +0800

    fix cmakelist

---
+++ leetcode/editor/cn/225-implement-stack-using-queues.cpp
@@ -0,0 +1,74 @@
+//使用队列实现栈的下列操作： 
+//
+// 
+// push(x) -- 元素 x 入栈 
+// pop() -- 移除栈顶元素 
+// top() -- 获取栈顶元素 
+// empty() -- 返回栈是否为空 
+// 
+//
+// 注意: 
+//
+// 
+// 你只能使用队列的基本操作-- 也就是 push to back, peek/pop from front, size, 和 is empty 这些操作是合
+//法的。 
+// 你所使用的语言也许不支持队列。 你可以使用 list 或者 deque（双端队列）来模拟一个队列 , 只要是标准的队列操作即可。 
+// 你可以假设所有操作都是有效的（例如, 对一个空的栈不会调用 pop 或者 top 操作）。 
+// 
+// Related Topics 栈 设计 
+// 👍 263 👎 0
+
+
+#include "include/headers.h"
+
+using namespace std;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class MyStack {
+public:
+    /** Initialize your data structure here. */
+    MyStack() {
+
+    }
+    
+    /** Push element x onto stack. */
+    void push(int x) {
+
+    }
+    
+    /** Removes the element on top of the stack and returns that element. */
+    int pop() {
+
+    }
+    
+    /** Get the top element. */
+    int top() {
+
+    }
+    
+    /** Returns whether the stack is empty. */
+    bool empty() {
+
+    }
+};
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack* obj = new MyStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->top();
+ * bool param_4 = obj->empty();
+ */
+//leetcode submit region end(Prohibit modification and deletion)
+
+
+int main()
+{
+    Solution s;
+    vector<int> data{7, 1, 5, 3, 6, 4};
+    //vector<int> ans = s.twoSum(data,11);
+    //cout << ans[0]<<ans[1]<<endl;
+    auto res = "Hello LeetCode";
+    cout<<res<<endl;
+}
---
+++ leetcode/editor/cn/628-maximum-product-of-three-numbers.cpp
@@ -0,0 +1,49 @@
+//给定一个整型数组，在数组中找出由三个数组成的最大乘积，并输出这个乘积。 
+//
+// 示例 1: 
+//
+// 
+//输入: [1,2,3]
+//输出: 6
+// 
+//
+// 示例 2: 
+//
+// 
+//输入: [1,2,3,4]
+//输出: 24
+// 
+//
+// 注意: 
+//
+// 
+// 给定的整型数组长度范围是[3,104]，数组中所有的元素范围是[-1000, 1000]。 
+// 输入的数组中任意三个数的乘积不会超出32位有符号整数的范围。 
+// 
+// Related Topics 数组 数学 
+// 👍 240 👎 0
+
+
+#include "include/headers.h"
+
+using namespace std;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+public:
+    int maximumProduct(vector<int>& nums) {
+
+    }
+};
+//leetcode submit region end(Prohibit modification and deletion)
+
+
+int main()
+{
+    Solution s;
+    vector<int> data{7, 1, 5, 3, 6, 4};
+    //vector<int> ans = s.twoSum(data,11);
+    //cout << ans[0]<<ans[1]<<endl;
+    auto res = "Hello LeetCode";
+    cout<<res<<endl;
+}
